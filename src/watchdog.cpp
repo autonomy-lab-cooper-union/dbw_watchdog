894c75d42c3801b3d634d04b4c9dc2ed21b9235c
dmezh <54985569+dmezh@users.noreply.github.com>
2021-02-25 23:22:43 -0500

    add nice console output

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -17,7 +17,10 @@ std::array<void(*)(const int), 1> modules
 
 int main()
 {
-    printf("Number of modules: %lu\n", modules.size());
+    LOGMSG(watchdog, 0, "Cooper Union IGVC DBW Watchdog v0.0.0a");
+    LOGMSG(watchdog, 0, "-----------------INIT-----------------");
+    LOGMSG(watchdog, 0, "Number of modules: %lu", modules.size());
+    LOGMSG(watchdog, 0, "launching modules...");
     std::vector<std::thread> threads;
     core::current_mode.store(core::modes::FULL_DRIVE);
     //printf("the current driving mode is %d\n", core::current_mode.load());
@@ -27,7 +30,6 @@ int main()
         threads.push_back(std::thread(modules.at(t), t));
     }
     //printf("the current driving mode is %d\n", core::current_mode.load());
-
     int buffer[modules.size()] = {0};
     int leave = 0;
 
@@ -41,14 +43,15 @@ int main()
         usleep(100 * 1000);
 
         for(int i=0; i < modules.size(); ++i) {
-            printf("buffer: thread %d, Buffer Value: %d, Status Value: %ld\n", i, buffer[i], core::status[i].load());
+            // printf("buffer: thread %d, Buffer Value: %d, Status Value: %ld\n", i, buffer[i], core::status[i].load());
             if (buffer[i] == core::status[i].load()) {
-                printf("Stopped due to thread %d\n", i);
+                LOGMSG(watchdog, 1, "Stopped due to dead thread: #%d", i);
                 goto exit;
             }
         }
     }
 exit:
+    LOGMSG(watchdog, 1, "**** EXITING ****");
     // when the program exits, ALL the threads die too
     exit(0);
 }
