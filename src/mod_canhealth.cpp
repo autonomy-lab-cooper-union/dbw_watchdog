efc657469193fbc8e1cea3599a1d457bbf459ad7
dmezh <54985569+dmezh@users.noreply.github.com>
2021-02-25 20:31:28 -0500

    add basic functionality for mod_canhealth

--- src/mod_canhealth.cpp
+++ src/mod_canhealth.cpp
@@ -1,4 +1,7 @@
+#include <net/if.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include "core.h"
 #include "mod_canhealth.h"
@@ -10,6 +13,16 @@ void mod_canhealth(const int THIS_THREAD)
     int count = 0;
     for (;;)
     {
+        struct ifreq ifr;
+        strncpy(ifr.ifr_name, "can0", IFNAMSIZ);
+        ifr.ifr_flags = IFF_UP|IFF_RUNNING|IFF_NOARP;
+        int fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+        if (int r = ioctl(fd, SIOCGIFFLAGS, &ifr) < 0) {
+                fprintf(stderr, "Error in ioctl! Error %d: %s\n", r, strerror(r));
+                return;
+        }
+        printf("interface is up: %d\n", (ifr.ifr_flags & IFF_UP));
+
         core::status[THIS_THREAD]++;
         usleep(1000);
         count++;
