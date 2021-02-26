3b26735ecb7b2df4279570a20daef81826a38b53
dmezh <54985569+dmezh@users.noreply.github.com>
2021-02-25 20:33:26 -0500

    do ioctl init only once

--- src/mod_canhealth.cpp
+++ src/mod_canhealth.cpp
@@ -11,12 +11,14 @@
 void mod_canhealth(const int THIS_THREAD)
 {
     int count = 0;
+    // set up stuff for ioctl
+    struct ifreq ifr;
+    strncpy(ifr.ifr_name, "can0", IFNAMSIZ);
+    ifr.ifr_flags = IFF_UP|IFF_RUNNING|IFF_NOARP;
+    int fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+
     for (;;)
     {
-        struct ifreq ifr;
-        strncpy(ifr.ifr_name, "can0", IFNAMSIZ);
-        ifr.ifr_flags = IFF_UP|IFF_RUNNING|IFF_NOARP;
-        int fd = socket(AF_UNIX, SOCK_DGRAM, 0);
         if (int r = ioctl(fd, SIOCGIFFLAGS, &ifr) < 0) {
                 fprintf(stderr, "Error in ioctl! Error %d: %s\n", r, strerror(r));
                 return;
