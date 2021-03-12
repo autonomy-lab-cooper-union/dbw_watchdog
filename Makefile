13ef80d64b4169cc1cc37033bfe1bf51dd45e616
dmezh <54985569+dmezh@users.noreply.github.com>
2021-03-12 14:44:35 -0500

    fix libsocketcan

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ obj = $(src:.cpp=.o)
 all: clean watchdog
 
 watchdog: $(obj)
-	$(CXX) -pthread -o $@ $^ /mnt/c/users/aligh/Desktop/hacker/IGVC/dbw_watchdog/src/libsocketcan/src/libsocketcan.o -latomic 
-	$(RM) $(obj)
+        $(CXX) -pthread -o $@ $^ -lsocketcan -latomic
+        $(RM) $(obj)
 
 clean:
-	$(RM) $(obj) watchdog
+        $(RM) $(obj) watchdog
--- src/mod_canhealth_lsc.cpp
+++ src/mod_canhealth_lsc.cpp
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <libsocketcan.h>
 #include "core.h"
 #include "mod_canhealth_lsc.h"
-#include "libsocketcan.h"
 
 // impl. functions in core to kick watchdog
 
 void mod_canhealth_lsc(const int THIS_THREAD)
 {
-    int* canState = 0;
+    int canState;
     int count = 0;
     LOGMSG(mod_canhealth, 0, "Running");
     for (;;)
     {
         
-        if (can_get_state("can0", canState) == -1) {
-            LOGMSG(mod_canhealth, 1, "CAN UNHEALTHY, calling estop()");
+        if (can_get_state("can0", &canState) < 0) {
+            LOGMSG(mod_canhealth, 1, "Error getting CAN state!");
             core::estop();
+        }
+        if (canState != CAN_STATE_ERROR_ACTIVE) {
+             LOGMSG(mod_canhealth, 1, "CAN UNHEALTHY, calling estop()");
         }
         core::status[THIS_THREAD]++;
         usleep(1000);
